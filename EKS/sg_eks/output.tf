b6aa263c40fcda271f37307e08649abe30962641
mateenshaik <mateenshaikh046@gmail.com>
2023-08-03 19:09:21 +0530

    EKS setup

--- EKS/sg_eks/output.tf
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-output "security_group_public" {
-   value = "${aws_security_group.worker_node_sg.id}"
-=======
-output "security_group_public" {
-   value = "${aws_security_group.worker_node_sg.id}"
->>>>>>> 27d358f89d37f2b9dee26fb336d18a65c0f34b42
-}

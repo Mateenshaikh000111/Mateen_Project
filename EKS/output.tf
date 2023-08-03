3cc90846de11221bcf4618b4a4819e40bb6d859c
Mateenshaikh000111 <139356532+Mateenshaikh000111@users.noreply.github.com>
2023-08-03 19:06:57 +0530

    Delete EKS directory
    no needed

--- EKS/output.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip_of_demo" {
-  description = "this is public IP"
-  value = aws_instance.demo.public_ip
-}
-
-output "private_ip_of_demo" {
-  description = "this is public IP"
-  value = aws_instance.demo.private_ip
-}
